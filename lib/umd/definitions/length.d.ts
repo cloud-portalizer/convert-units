faf334dc19d162ae1ed7fe903f3b6c7f2002a806
Andreas Caldewei <68024440+AndreasCaldewei@users.noreply.github.com>
2022-01-04 13:40:52 +0100

    compile

--- lib/umd/definitions/length.d.ts
+++ lib/umd/definitions/length.d.ts
@@ -1,7 +1,7 @@
 import { Measure } from './../index';
 export declare type LengthUnits = LengthMetricUnits | LengthImperialUnits;
 export declare type LengthSystems = 'metric' | 'imperial';
-export declare type LengthMetricUnits = 'nm' | 'μm' | 'mm' | 'cm' | 'm' | 'km';
+export declare type LengthMetricUnits = 'nm' | 'μm' | 'mm' | 'cm' | 'm' | 'km' | 'px';
 export declare type LengthImperialUnits = 'in' | 'yd' | 'ft-us' | 'ft' | 'fathom' | 'mi' | 'nMi';
 declare const measure: Measure<LengthSystems, LengthUnits>;
 export default measure;
